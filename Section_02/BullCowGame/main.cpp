f5a4cc4972af2cd5507fbc2efed2490242aef89f
Vandré Brunazo <vibrunazo@gmail.com>
2018-12-02 16:17:12 -0200

    'l24'

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -2,22 +2,45 @@
 #include <string>
 using namespace std;
 
+void PrintIntro();
+string GetGuess();
+void PrintGuess(string);
+
+// the entry point for our app
 int main() {
-	constexpr int WORLD_LENGTH = 5;
 
-	// introduce the game
-	cout << "Welcome to Bulls and Cows" << endl;
-	cout << "Can you guess the " << WORLD_LENGTH << " letter isogram I'm thinking of?\n";
-
-	// get a guess from the player
+	PrintIntro();
+	
 	string Guess = "";
-	// cin >> Guess;
-	getline(cin, Guess);
+	
+	Guess = GetGuess();
+	PrintGuess(Guess);
 
-	// repeat the guess back to them
-	cout << "your guess was: ";
-	cout << Guess;
-
+	Guess = GetGuess();
+	PrintGuess(Guess);
 
 	return 0;
+}
+
+// introduce the game
+void PrintIntro() {
+	constexpr int WORLD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows" << endl;
+	cout << "Can you guess the " << WORLD_LENGTH << " letter isogram I'm thinking of?\n\n";
+	return;
+}
+
+// get a guess from the player
+string GetGuess() {
+	string Guess = "";
+	cout << "Enter your guess\n";
+	getline(cin, Guess);
+	return Guess;
+}
+
+// repeat the guess back to them
+void PrintGuess(string guess)
+{
+	cout << "your guess was: ";
+	cout << guess << endl << endl;
 }
